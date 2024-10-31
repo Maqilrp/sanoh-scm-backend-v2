b0b83f6acb2a2718e06934d51d5e12ffaae42681
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2024-10-31 14:48:02 +0700

    fix: add condition to auth response
    get partner name from value adr_line_1, but if null get bp_name instead

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -63,7 +63,7 @@ class AuthController
             'access_token' => $token,
             'role' => $user->role,
             'bp_code' => $user->bp_code,
-            'name' => $user->partner->adr_line_1,
+            'name' => ($user->partner->adr_line_1 == null) ? $user->partner->bp_name : $user->partner->adr_line_1,
             'token_type' => 'Bearer'
         ]);
     }
