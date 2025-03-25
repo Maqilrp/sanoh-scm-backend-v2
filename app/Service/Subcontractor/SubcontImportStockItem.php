33385ff0090917b25ad1b66e43c0516a4d27d254
Muhammad_Aqil_Rasyid_Permana <aqilrasyid2004@gmail.com>
2025-03-25 11:35:24 +0700

    refactor: integrate ErrorLog and ResponseApi traits in SubcontImportStockItem; enhance error handling and response for stock import process

--- app/Service/Subcontractor/SubcontImportStockItem.php
+++ app/Service/Subcontractor/SubcontImportStockItem.php
@@ -2,16 +2,25 @@
 
 namespace App\Service\Subcontractor;
 
+use App\Trait\ErrorLog;
+use Log;
+use Carbon\Carbon;
+use App\Trait\ResponseApi;
+use Illuminate\Support\Facades\DB;
 use App\Models\Subcontractor\SubcontItem;
 use App\Models\Subcontractor\SubcontStock;
-use Carbon\Carbon;
 use Illuminate\Http\Exceptions\HttpResponseException;
-use Illuminate\Support\Facades\DB;
-use Log;
 
 class SubcontImportStockItem
 {
-    public function __construct(protected SubcontCreateStock $subcontCreateStock) {}
+    /**
+     * -------TRAIT---------
+     * Mandatory:
+     * 1. ResponseApi = Response api should use ResponseApi trait template
+     * 2. ErrorLog = Make internal log and return RequestId if the logic error was critical
+     */
+    use ResponseApi, ErrorLog;
+    public function __construct(protected SubcontCreateStock $subcontCreateStock){}
 
     public function importStockItem(
         string $bpCode,
@@ -23,61 +32,39 @@ class SubcontImportStockItem
         int $replatingProcessItems,
         int $replatingNgItems,
     ) {
-        try {
-            // Query to get sub_item_id form table subcont_item
-            $getItem = SubcontItem::where('bp_code', $bpCode)
-                ->where('item_code', $partNumber)
-                ->value('sub_item_id');
+        // Query to get sub_item_id form table subcont_item
+        $getItem = SubcontItem::where('bp_code', $bpCode)
+            ->where('item_code', $partNumber)
+            ->value('sub_item_id');
+        if (!$getItem) {
+            return $this->returnResponseApi(false, 'Subcont Item Not Found', 404);
+        }
+        // Check stock record availability
+        $this->subcontCreateStock->createAndCheckStock($partNumber, $getItem);
 
-            // Check stock record availability
-            $this->subcontCreateStock->createAndCheckStock($partNumber, $getItem);
-
-            // Query to get stock record form table subcont_stock
-            $getStock = SubcontStock::where('sub_item_id', $getItem)
-                ->where('item_code', $partNumber)
-                ->first();
-
-            // Update/import stock
-            DB::transaction(function () use (
-                $getStock,
-                $freshIncomingItems,
-                $freshProcessItems,
-                $freshNgItems,
-                $replatingIncomingItems,
-                $replatingProcessItems,
-                $replatingNgItems,
-            ) {
-                $getStock->update([
-                    'incoming_fresh_stock' => $freshIncomingItems,
-                    'process_fresh_stock' => $freshProcessItems,
-                    'ng_fresh_stock' => $freshNgItems,
-                    'incoming_replating_stock' => $replatingIncomingItems,
-                    'process_replating_stock' => $replatingProcessItems,
-                    'ng_replating_stock' => $replatingNgItems,
-                ]);
-            });
-        } catch (\Throwable $th) {
-            // Generate random request id
-            $randomReqId = 'error_'.Carbon::now()->format('Ymd;H:i:s').'_'.\Str::random(10);
-
-            // Log error to channel internal system error
-            Log::error("
-                Message => Input Import Stock Data Error,
-                Error => {$th->getMessage()},
-                File => {$th->getFile()},
-                Line => {$th->getLine()},
-                RequestId => $randomReqId,
-            ");
-
-            // Response
-            throw new HttpResponseException(
-                response()->json([
-                    'status' => false,
-                    'message' => "Internal error while input stock (Request_id:$randomReqId)",
-                    'error' => "Internal error while input stock (Request_id:$randomReqId)",
-                ], 500)
-            );
+        // Query to get stock record form table subcont_stock
+        $getStock = SubcontStock::where('sub_item_id', $getItem)
+            ->where('item_code', $partNumber)
+            ->first();
+        if (!$getStock) {
+            return $this->returnResponseApi(false, 'Subcont Stock Not Found', 404);
         }
 
+        // Update/import stock
+        try {
+            $getStock->update([
+                'incoming_fresh_stock' => $freshIncomingItems,
+                'process_fresh_stock' => $freshProcessItems,
+                'ng_fresh_stock' => $freshNgItems,
+                'incoming_replating_stock' => $replatingIncomingItems,
+                'process_replating_stock' => $replatingProcessItems,
+                'ng_replating_stock' => $replatingNgItems,
+            ]);
+        } catch (\Throwable $th) {
+            $requestId = $this->logicError('Error Import Item Subcont', $th->getMessage(), $th->getFile(), $th->getLine());
+            return $this->returnResponseApi(false, "Internal error while input stock (Request_id:$requestId)", null, 500);
+        }
+
+        return $this->returnResponseApi(true, 'Import Subcont Item Successful', null, 201);
     }
 }
